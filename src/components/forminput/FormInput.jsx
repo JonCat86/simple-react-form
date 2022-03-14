706bfd0e4d742051dadc550996b3fc0ab9f289aa
Jonathan <joncat86@gmail.com>
2022-03-14 17:57:05 +0100

    css to scss

--- src/components/forminput/FormInput.jsx
+++ src/components/forminput/FormInput.jsx
@@ -10,7 +10,7 @@ const FormInput = (props) => {
   };
 
   return (
-    <div className="formInput">
+    <div className="forminput">
       <label>{label}</label>
       <input
         {...inputProps}
